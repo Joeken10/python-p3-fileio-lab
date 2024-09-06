e28c3b1314a259d6357ab36b7cfc08a50acf0062
Joeken10 <joekentafir@gmail.com>
2024-09-06 18:39:53 +0300

    add files

--- lib/file_io.py
+++ lib/file_io.py
@@ -1,5 +1,7 @@
 def write_file(file_name, file_content):
-    pass
+     with open(f"{file_name}.txt", 'w') as file:
+        file.write(file_content)
+        pass
 
 def append_file(file_name, append_content):
     pass
